3742c39449e425621a972fd8146aa63787148e5c
Kyle Gough <kylegough98@gmail.com>
2022-01-23 21:10:33 +0000

    Fixed image modal, removed proxy

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -21,7 +21,7 @@ export default function ImageModal(props) {
             <div onClick={closeModal} className='fixed bg-opacity-75 h-screen w-full left-0 top-0 flex justify-center items-center bg-black'>
                 <div className='bg-white rounded shadow-lg'> 
                     <div className='p-1 items-center justify-center text-center'>
-                        <img className='text-center border-2 border-link max-w-screen-xl' src={props.src} alt={props.caption} />
+                        <img className='text-center border-2 border-link max-w-full lg:max-w-screen-lg' src={props.src} alt={props.caption} />
                     </div>
                     <div className="items-center text-center w-100 border-t p-3">
                         <p className='my-1 text-sm text-center font-thin'>{props.caption}</p>
