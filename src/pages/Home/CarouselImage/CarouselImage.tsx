83672f2bfbf49e4478a664a58a969635c0b3fe14
Kyle Gough <kylegough98@gmail.com>
2022-09-22 00:28:33 +0100

    Fixed homepage carousel

--- src/pages/Home/CarouselImage/CarouselImage.tsx
+++ src/pages/Home/CarouselImage/CarouselImage.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 
 interface CarouselImageProps {
   show: boolean;
-  className?: string;
+  bg: string;
 }
 
-export const CarouselImage: React.FC<CarouselImageProps> = ({
-  show,
-  className,
-}) => (
+export const CarouselImage: React.FC<CarouselImageProps> = ({ show, bg }) => (
   <div
-    className={`${className} ${
+    className={`${bg} ${
       show ? 'opacity-100' : 'opacity-0'
     } absolute top-0 left-0 w-full h-full bg-fixed bg-center bg-cover duration-2000 transition-opacity`}
   ></div>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -37,13 +37,19 @@ export const Home: React.FC = () => {
     setCarousel((carousel) => (carousel + 1) % carouselCount);
   };
 
+  const carouselImages = {
+    cave: 'bg-header1',
+    minesweeper: 'bg-header2',
+    coaster: 'bg-header3',
+  };
+
   return (
     <>
       <div className="flex justify-center items-center h-screen relative">
         <div className="absolute inset-0 bg-black opacity-70 w-full h-full z-10"></div>
-        <CarouselImage className="bg-header-1" show={carousel === 0} />
-        <CarouselImage className="bg-header-2" show={carousel === 1} />
-        <CarouselImage className="bg-header-3" show={carousel === 2} />
+        <CarouselImage bg={carouselImages.cave} show={carousel === 0} />
+        <CarouselImage bg={carouselImages.minesweeper} show={carousel === 1} />
+        <CarouselImage bg={carouselImages.coaster} show={carousel === 2} />
         <div className="z-20 absolute inset-0 text-white mx-auto text-center flex tracking-tight items-center">
           <div className="block lg:flex items-center justify-around w-full">
             <h1 className="slide-in text-7xl sm:text-8xl lg:text-9xl font-sans tracking-tighter px-8 mb-12 lg:mb-0">
