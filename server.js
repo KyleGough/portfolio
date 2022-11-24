3708ee43eb6a2c3487f3eec39245e0ceb9bb60e0
Kyle Gough <kylegough98@gmail.com>
2022-11-24 10:41:57 +0000

    Modify CSP to allow Vercel

--- server.js
+++ server.js
@@ -8,7 +8,29 @@ const app = express();
 const sgMail = require('@sendgrid/mail');
 const cors = require('cors');
 
-app.use(helmet());
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "default-src": ["'self'", "vitals.vercel-insights.com"]
+    },
+  })
+);
+app.use(helmet.crossOriginEmbedderPolicy());
+app.use(helmet.crossOriginOpenerPolicy());
+app.use(helmet.crossOriginResourcePolicy());
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.originAgentCluster());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
+
 app.use(cors());
 app.use(compression());
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
