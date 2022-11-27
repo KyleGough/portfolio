298c7a4813df6e212353bda65ee39e10f401cde2
Kyle Gough <kylegough98@gmail.com>
2022-11-27 23:59:03 +0000

    Test header refinement

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const contentSecurityPolicy = `
   default-src 'none';
   script-src-elem 'self';
+  script-src 'unsafe-eval';
 `;
 
 // "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests"
