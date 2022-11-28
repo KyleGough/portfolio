3ff99797c8149cc3a0bb3f6e37b7ee2ad2bdf354
Kyle Gough <kylegough98@gmail.com>
2022-11-28 15:40:23 +0000

    Updated Logos (#105)

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -26,11 +26,6 @@ export const Layout: React.FC<LayoutProps> = ({
           content="Kyle, Gough, Portfolio, Project, Programmer, Web, Developer, Python, Sorting, Visualiser"
         />
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
@@ -42,9 +37,13 @@ export const Layout: React.FC<LayoutProps> = ({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
         <link crossOrigin="true" rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:title" content="Kyle Gough - Portfolio" />
