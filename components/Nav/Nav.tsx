f771e9b4820b880b4e196d41b4d72ca223d54476
Kyle Gough <kylegough98@gmail.com>
2022-11-28 16:13:23 +0000

    Added logo to nav

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,5 +1,6 @@
 import { GitHubIcon, MenuIcon } from '@components/Icons';
 import { clsx } from 'clsx';
+import Image from 'next/image';
 import Link from 'next/link';
 import React, { useEffect, useRef, useState } from 'react';
 
@@ -35,8 +36,16 @@ export const Nav: React.FC = () => {
     <nav className="leading-6 w-full absolute z-50">
       <div className="flex relative z-[200] items-center justify-between flex-wrap bg-nav text-white px-2">
         <div className="flex items-center flex-shrink-0 mr-6">
+          <div className="ml-6 w-8 h-8">
+            <Image
+              src="/favicon-32x32.png"
+              alt="Website Logo"
+              width={30}
+              height={30}
+            />
+          </div>
           <Link href="/">
-            <a className="ml-6">
+            <a className="ml-3">
               <span className="text-xl tracking-normal">Kyle Gough</span>
             </a>
           </Link>
