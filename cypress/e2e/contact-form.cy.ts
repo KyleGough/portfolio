c5c5b2388101a7321d7e7d26dd11e55c6c80e136
Kyle Gough <kylegough98@gmail.com>
2022-11-30 00:08:56 +0000

    Cypress e2e tests

---
+++ cypress/e2e/contact-form.cy.ts
@@ -0,0 +1,24 @@
+import dayjs from 'dayjs';
+
+describe('Contact Form', () => {
+  it('Submit contact form with valid details', () => {
+    cy.intercept('POST', '/api/contact', { success: true }).as('sendMessage');
+    cy.visit('/');
+    cy.get('#name').focus().type('Kyle Gough');
+    cy.get('#email').focus().type('kylegough98@gmail.com');
+    cy.get('#message').focus().type(`e2e contact form test at ${dayjs()}.`);
+    cy.getSubmitButton().click();
+    cy.wait('@sendMessage');
+    cy.getSubmitButton().should('have.text', 'Message Sent');
+  });
+
+  it('Submit contact form with invalid details', () => {
+    cy.visit('/');
+    cy.getSubmitButton().click();
+    cy.getFormLabel('name').should('have.text', 'Required!');
+    cy.getFormLabel('email').should('have.text', 'Required!');
+    cy.getFormLabel('message').should('have.text', 'Required!');
+  });
+});
+
+export {};
---
+++ cypress/e2e/navigation-desktop.cy.ts
@@ -0,0 +1,31 @@
+describe('Desktop Navigation', () => {
+  it('Navigate to about page', () => {
+    cy.visit('/');
+    cy.get('nav a[href="/about"]:visible').click();
+    cy.url().should('include', '/about');
+    cy.get('h1').should('have.text', 'About Me');
+  });
+
+  it('Navigate to projects page', () => {
+    cy.visit('/');
+    cy.get('nav a[href="/projects"]:visible').click();
+    cy.url().should('include', '/projects');
+    cy.get('h1').should('have.text', 'Projects');
+  });
+
+  it('Navigate to homepage', () => {
+    cy.visit('/about');
+    cy.get('nav a[href="/"]:visible').contains('Kyle Gough').click();
+    cy.url().should('include', '/');
+    cy.get('h1').should('have.text', "Hello, I'm Kyle.");
+  });
+
+  it('Navigate to homepage', () => {
+    cy.visit('/about');
+    cy.get('nav a[href="/"]:visible').contains('Home').click();
+    cy.url().should('include', '/');
+    cy.get('h1').should('have.text', "Hello, I'm Kyle.");
+  });
+});
+
+export {};
