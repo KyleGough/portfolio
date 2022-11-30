859be7066ea97892a3cfc2e72d0652a3c8807b5c
Kyle Gough <kylegough98@gmail.com>
2022-11-30 16:02:48 +0000

    Integrated Vercel Analytics (#134)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import 'styles/globals.css';
 import 'styles/tailwind.css';
 
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
