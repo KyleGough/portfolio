d174bb917b9513045a90ec20850550e8eaab53f4
Kyle Gough <kylegough98@gmail.com>
2022-12-02 13:02:57 +0000

    Improved colour rules on contact form

--- components/Contact/Contact.helper.ts
+++ components/Contact/Contact.helper.ts
@@ -1,14 +1,13 @@
-import { clsx } from 'clsx';
-
 export const validateEmail = (email: string) => {
   const emailRegex =
     /^(([^\s"(),.:;<>@[\\\]]+(\.[^\s"(),.:;<>@[\\\]]+)*)|(".+"))@((\[(?:\d{1,3}\.){3}\d{1,3}])|(([\dA-Za-z-]+\.)+[A-Za-z]{2,}))$/;
   return !email || emailRegex.test(String(email).toLowerCase());
 };
 
-export const getFieldBorderStyle = (hasError: boolean) => {
-  return clsx(
-    { 'border-error': hasError },
-    { 'border-link focus:border-link-hover': !hasError }
-  );
+export const getFieldBorderStyle = (valid: boolean, sent: boolean) => {
+  if (sent) {
+    return 'border-link-hover';
+  } else {
+    return valid ? 'border-link focus:border-link-hover' : 'border-error';
+  }
 };
--- components/Contact/ContactLabel/ContactLabel.tsx
+++ components/Contact/ContactLabel/ContactLabel.tsx
@@ -3,19 +3,22 @@ import React from 'react';
 
 interface ContactLabelProps {
   valid: boolean;
+  isSent: boolean;
   htmlFor: string;
   children: React.ReactNode;
 }
 
 export const ContactLabel: React.FC<ContactLabelProps> = ({
   valid,
+  isSent,
   htmlFor,
   children,
 }) => (
   <label
     className={clsx(
-      { 'text-link': valid },
-      { 'text-error': !valid },
+      { 'text-link-hover': isSent },
+      { 'text-link group-focus-within:text-link-hover': valid && !isSent },
+      { 'text-error': !valid && !isSent },
       'transition-colours',
       'duration-200',
       'text-lg',
