3c47c61c6cb4212b0e5287f2c1c21f4b88e01739
Kyle Gough <kylegough98@gmail.com>
2023-06-22 11:46:08 +0100

    Server validation of send endpoint

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,4 +1,5 @@
 import { EmailTemplate } from '@components/EmailTemplate';
+import joi from 'joi';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Resend } from 'resend';
 
@@ -10,9 +11,25 @@ interface Email {
 
 const resend = new Resend(process.env.EMAIL_API_KEY);
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+const send = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method || 'UNKNOWN'} Not Allowed`);
+    return;
+  }
+
+  const schema = joi.object({
+    email: joi.string().email().max(254).required(),
+    message: joi.string().max(1024).required(),
+    name: joi.string().max(50).required(),
+  });
+
   try {
     const body = req.body as Email;
+    const { error } = schema.validate(req.body);
+    if (error) {
+      throw error;
+    }
 
     const data = await resend.emails.send({
       from: 'contact@kylegough.co.uk',
@@ -27,3 +44,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     res.status(400).json(error);
   }
 };
+
+export default send;
+
+// POST only.
