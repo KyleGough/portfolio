061b4b1e12b44fa8bafb43d917969afa7f8be121
Kyle Gough <kylegough98@gmail.com>
2026-05-08 16:38:43 +0100

    SVH polyfill (#299)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { fontLato, fontMerriweather } from '@utilities/siteFonts';
+import { VIEWPORT_UNIT_POLYFILL_INLINE } from '@utilities/viewportUnitPolyfill';
 import { clsx } from 'clsx';
 import { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
@@ -9,7 +10,12 @@ export default function Document() {
       lang="en"
       className={clsx(fontLato.variable, fontMerriweather.variable)}
     >
-      <Head />
+      <Head>
+        {/* Runs before React hydrate; sets --vp-svh / --vp-lvh / --vp-dvh when svh is unsupported */}
+        <script
+          dangerouslySetInnerHTML={{ __html: VIEWPORT_UNIT_POLYFILL_INLINE }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
